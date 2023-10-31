299b3bbe47600cbb47faec2728e6683c49b0939d
MickeyMemeDev <148511289+MickeyMemeDev@users.noreply.github.com>
2023-10-31 00:22:04 +0000

    working

---
+++ lib/links.ts
@@ -0,0 +1,22 @@
+export const navLinks = [
+  {
+    route: "Home",
+    path: "/",
+  },
+  {
+    route: "Uniswap",
+    path: "https://app.uniswap.org/swap?inputCurrency=0x7391a131ccb43a571a34e09f986080d117b4313c&outputCurrency=0xc02aaa39b223fe8d0a0e5c4f27ead9083c756cc2",
+  },
+  {
+    route: "Contract",
+    path: "https://etherscan.io/token/0x7391a131cCb43a571a34e09f986080D117b4313C",
+  },
+  {
+    route: "Discord",
+    path: "https://etherscan.io/token/0x7391a131cCb43a571a34e09f986080D117b4313C",
+  },
+  // {
+  //   route: "Github",
+  //   path: "https://github.com/mickeymemecoin",
+  // },
+]
