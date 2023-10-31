ab4416355e3b9e6ac64078f8e0c076f1325690de
MickeyMemeDev <148511289+MickeyMemeDev@users.noreply.github.com>
2023-10-31 21:58:45 +0000

    add components/pages/HollywoodHero.tsx stub

---
+++ components/pages/HollywoodHero.tsx
@@ -0,0 +1,18 @@
+// import Link from "next/link"
+// import Image from "next/image"
+// import { buttonVariants } from "@/components/ui/button"
+// import { cn } from "@/lib/utils"
+// import { heroHeader } from "@/config/contents"
+// import { Unipop } from "./Unipop"
+
+export default function HeroSection() {
+  return (
+    // TODO: add bg image to section
+    <section className="container flex flex-col gap-4 pb-12 pt-4 text-center lg:items-center lg:gap-8 lg:py-20">
+      <div className="flex flex-1 flex-col items-center gap-4 text-center lg:gap-8">
+        {/* TODO: add mickey coin logotype */}
+        {/* TODO: add call to action */}
+      </div>
+    </section>
+  )
+}
