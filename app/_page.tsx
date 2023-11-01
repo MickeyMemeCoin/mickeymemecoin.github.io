4519b9f60b32fd584812bf4cdd2854e681bd7d2f
MickeyMemeDev <148511289+MickeyMemeDev@users.noreply.github.com>
2023-11-01 04:23:44 +0000

    promote demo to homepage

---
+++ app/_page.tsx
@@ -0,0 +1,13 @@
+import SplashHero from "@/components/pages/SplashHero"
+import FeatureCards from "@/components/pages/feature-cards"
+import Features from "@/components/pages/features"
+
+export default function Home() {
+  return (
+    <main>
+      <SplashHero />
+      {/* <FeatureCards /> */}
+      {/* <Features /> */}
+    </main>
+  )
+}
